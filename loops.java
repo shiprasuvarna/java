ab8d44b8b29b3eb4f48100613cfd9e537739aa05
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-10-03 19:38:39 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,27 @@
+////while loop
+//package javaapplication1;
+//public class loops {
+//    public static void main(String[] args) {
+//        int i;
+//        i=1;
+//        while(i<=10)
+//        {
+//            System.out.println(i);
+//        i++;
+//        }
+//    }
+//    
+//}
+
+public class loops{
+    public static void main(String[] args) {
+        int i;
+        i=1;
+        do
+        {
+            System.out.println(i);
+            i++;
+        }
+        while(i<=10);
+    }
+}
