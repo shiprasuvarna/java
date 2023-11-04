a20eb9eb337376d967c6d4b6f2a7a26b9c7d681b
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-11-04 22:52:18 +0530

    Add files via upload

---
+++ OneDimensionalArray.java
@@ -0,0 +1,14 @@
+//incomplete
+package javaapplication1;
+
+public class OneDimensionalArray {
+    public static void main(String[] args) {
+        String student_name[]={"Shipra", "Ship", "Shira", "Shakira", "Shanaya"};
+        String[] student_name=new String[5];
+        student_name[0]="Shipra";
+        student_name[1]="Ship";
+        student_name[2]="Shira";
+        student_name[3]="Shakira";
+        student_name[4]="Shanaya";
+    }
+}
