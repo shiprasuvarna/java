5472414d73cf024a8b73f4a82981a35ff3cc0ab3
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-11-04 22:53:15 +0530

    Add files via upload

---
+++ reverse_no.java
@@ -0,0 +1,13 @@
+//Enter a number & print it's reverse
+import java.util.Scanner;
+package javaapplication1;
+
+public class reverse_no {
+    public static void main(String[] args) {
+        int i, n, r;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        n=sc.nextInt();
+    }
+    
+}
