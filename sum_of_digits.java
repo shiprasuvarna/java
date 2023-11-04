25fa7a1450bd457162a67646cb5c1b90f587c566
Shipra Suvarna <shipraasuvarna@gmail.com>
2023-11-04 23:00:20 +0530

    Add files via upload

---
+++ sum_of_digits.java
@@ -0,0 +1,14 @@
+//Write a program to calculate sum of digits of a number.  
+import java.util.Scanner;
+package javaapplication1;
+
+public class sum_of_digits {
+    public static void main(String[] args) {
+        int i, n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        n=sc.nextInt();
+        
+    }
+    
+}
