a892543bcb5769b037774263808c7791e196d621
Shipra Suvarna <shipraasuvarna@gmail.com>
2024-08-26 20:33:28 +0530

    Update sum_of_digits.java

--- sum_of_digits.java
+++ sum_of_digits.java
@@ -4,11 +4,19 @@ package javaapplication1;
 
 public class sum_of_digits {
     public static void main(String[] args) {
-        int i, n;
+        int i, n, digit, sum=0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number");
         n=sc.nextInt();
-        
+         while(n > 0)  
+{  
+digit = n % 10;  
+sum = sum + digit;  
+n = n / 10;  
+}  
+System.out.println("Sum of Digits: "+sum);  
+}  
+}  
     }
     
 }
